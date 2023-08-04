1de28c025e0a6284102fc23f5053386051631b7c
Donghun Shin <huipulci1@naver.com>
2023-08-04 22:39:32 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/mallang/MallangLogApplication.java
@@ -0,0 +1,12 @@
+package com.mallang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallangLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallangLogApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mallang/MallangLogApplicationTests.java
@@ -0,0 +1,12 @@
+package com.mallang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallangLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
