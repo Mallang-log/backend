888eeef4019f941a17dbba45b3f32d12cbb9bdf9
shin-mallang <huipulci1@naver.com>
2023-08-05 23:05:53 +0900

    [#7] feat: 자신보다 하위의 카테고리를 부모로 둘 수 없도록 도메인 로직을 작성한다

--- src/main/java/com/mallang/category/domain/Category.java
+++ src/main/java/com/mallang/category/domain/Category.java
@@ -3,12 +3,16 @@ package com.mallang.category.domain;
 import static jakarta.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.PROTECTED;
 
-import com.mallang.category.application.exception.NoAuthorityUseCategory;
+import com.mallang.category.exception.CategoryHierarchyViolationException;
+import com.mallang.category.exception.NoAuthorityUseCategoryException;
 import com.mallang.common.domain.CommonDomainModel;
 import com.mallang.member.domain.Member;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,6 +32,9 @@ public class Category extends CommonDomainModel {
     @JoinColumn(name = "parent_id")
     private Category parent;
 
+    @OneToMany(fetch = LAZY, mappedBy = "parent")
+    private final List<Category> children = new ArrayList<>();
+
     @Builder
     public Category(String name, Member member) {
         this.name = name;
@@ -36,12 +43,28 @@ public class Category extends CommonDomainModel {
 
     public void setParent(Category parent) {
         validateSameMember(parent);
+        validateHierarchy(parent);
         this.parent = parent;
+        parent.addChild(this);
     }
+
 
     private void validateSameMember(Category parent) {
         if (!parent.getMember().getId().equals(member.getId())) {
-            throw new NoAuthorityUseCategory();
+            throw new NoAuthorityUseCategoryException();
         }
+    }
+
+    private void validateHierarchy(Category parent) {
+        if (children.contains(parent)) {
+            throw new CategoryHierarchyViolationException();
+        }
+        for (Category child : children) {
+            child.validateHierarchy(parent);
+        }
+    }
+
+    private void addChild(Category child) {
+        this.children.add(child);
     }
 }
--- src/test/java/com/mallang/category/domain/CategoryTest.java
+++ src/test/java/com/mallang/category/domain/CategoryTest.java
@@ -4,7 +4,8 @@ import static com.mallang.member.MemberFixture.memberBuilder;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import com.mallang.category.application.exception.NoAuthorityUseCategory;
+import com.mallang.category.exception.CategoryHierarchyViolationException;
+import com.mallang.category.exception.NoAuthorityUseCategoryException;
 import com.mallang.member.domain.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -53,9 +54,60 @@ class CategoryTest {
         // when
         assertThatThrownBy(() ->
                 하위.setParent(최상위)
-        ).isInstanceOf(NoAuthorityUseCategory.class);
+        ).isInstanceOf(NoAuthorityUseCategoryException.class);
 
         // then
         assertThat(하위.getParent()).isNull();
+    }
+
+    @Test
+    void 무한_Depth_가_가능하다() {
+        Member member1 = memberBuilder().id(1L).nickname("mallang").build();
+        Category 최상위 = Category.builder()
+                .name("최상위")
+                .member(member1)
+                .build();
+        Category 하위 = Category.builder()
+                .name("하위")
+                .member(member1)
+                .build();
+        Category 더하위 = Category.builder()
+                .name("더하위")
+                .member(member1)
+                .build();
+        하위.setParent(최상위);
+
+        // when
+        더하위.setParent(하위);
+
+        // then
+        assertThat(하위.getParent()).isEqualTo(최상위);
+    }
+
+    @Test
+    void 자신보다_낮은_Category_를_부모로_둘_수_없다() {
+        Member member1 = memberBuilder().id(1L).nickname("mallang").build();
+        Category 최상위 = Category.builder()
+                .name("최상위")
+                .member(member1)
+                .build();
+        Category 하위 = Category.builder()
+                .name("하위")
+                .member(member1)
+                .build();
+        Category 더하위 = Category.builder()
+                .name("더하위")
+                .member(member1)
+                .build();
+        하위.setParent(최상위);
+        더하위.setParent(하위);
+
+        // when & then
+        assertThatThrownBy(() -> 최상위.setParent(하위))
+                .isInstanceOf(CategoryHierarchyViolationException.class);
+        assertThatThrownBy(() -> 최상위.setParent(더하위))
+                .isInstanceOf(CategoryHierarchyViolationException.class);
+        assertThatThrownBy(() -> 하위.setParent(더하위))
+                .isInstanceOf(CategoryHierarchyViolationException.class);
     }
 }
