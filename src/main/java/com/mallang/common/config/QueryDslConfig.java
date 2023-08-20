945a5d5911d3479672941f599261745caef6d823
shin-mallang <huipulci1@naver.com>
2023-08-20 22:36:52 +0900

    [#9] chore: querydsl 설정

---
+++ src/main/java/com/mallang/common/config/QueryDslConfig.java
@@ -0,0 +1,19 @@
+package com.mallang.common.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class QueryDslConfig {
+
+    private final EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+}
