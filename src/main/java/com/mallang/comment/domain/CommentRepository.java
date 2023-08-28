e206ce9ab86153399fddd8a7a6dbca7e2bfc9433
shin-mallang <huipulci1@naver.com>
2023-08-28 18:08:55 +0900

    [#10] fix: 댓글 없을때 예외처리

--- src/main/java/com/mallang/comment/domain/CommentRepository.java
+++ src/main/java/com/mallang/comment/domain/CommentRepository.java
@@ -1,10 +1,12 @@
 package com.mallang.comment.domain;
 
+import com.mallang.comment.exception.NotFoundCommentException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     default Comment getById(Long id) {
-        return findById(id).orElseThrow();
+        return findById(id)
+                .orElseThrow(NotFoundCommentException::new);
     }
 }
