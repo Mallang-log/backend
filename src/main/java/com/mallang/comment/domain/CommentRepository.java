ee2872984e74b5403bf604d23357c96ea2c3c98b
shin-mallang <huipulci1@naver.com>
2023-08-29 16:38:04 +0900

    [#10] refactor: 댓글 조회시 작성자 fetch join

--- src/main/java/com/mallang/comment/domain/CommentRepository.java
+++ src/main/java/com/mallang/comment/domain/CommentRepository.java
@@ -1,6 +1,8 @@
 package com.mallang.comment.domain;
 
 import com.mallang.comment.exception.NotFoundCommentException;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
@@ -9,4 +11,8 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
         return findById(id)
                 .orElseThrow(NotFoundCommentException::new);
     }
+
+    @Override
+    @EntityGraph(attributePaths = "commentWriter")
+    Optional<Comment> findById(Long id);
 }
