a291d3001797efdd2fee4d0b2d3a022550b22759
shin-mallang <huipulci1@naver.com>
2023-10-24 12:32:26 +0900

    [#40] refactor: CategoryValidatorTest 리팩토링

---
+++ src/test/java/com/mallang/category/CategoryFixture.java
@@ -0,0 +1,30 @@
+package com.mallang.category;
+
+import static com.mallang.category.domain.AlwaysPassCategoryValidator.alwaysPassCategoryValidator;
+
+import com.mallang.category.domain.Category;
+import com.mallang.member.domain.Member;
+import org.springframework.test.util.ReflectionTestUtils;
+
+public class CategoryFixture {
+
+    public static Category 루트_카테고리(String name, Member member) {
+        return 루트_카테고리(null, name, member);
+    }
+
+    public static Category 루트_카테고리(Long id, String name, Member member) {
+        Category category = Category.create(name, member, null, alwaysPassCategoryValidator);
+        ReflectionTestUtils.setField(category, "id" , id);
+        return category;
+    }
+
+    public static Category 하위_카테고리(String name, Member member, Category parent) {
+        return 하위_카테고리(null, name, member, parent);
+    }
+
+    public static Category 하위_카테고리(Long id, String name, Member member, Category parent) {
+        Category category = Category.create(name, member, parent, alwaysPassCategoryValidator);
+        ReflectionTestUtils.setField(category, "id" , id);
+        return category;
+    }
+}
