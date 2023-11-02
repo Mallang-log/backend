04ddb07b9aa0128bce271c257a0dfe2e2ba84464
shin-mallang <huipulci1@naver.com>
2023-11-02 11:46:27 +0900

    [#11] feat: 블로그 구독 도메인(BlogSubscribe) 추가

---
+++ src/main/java/com/mallang/subscribe/domain/BlogSubscribe.java
@@ -0,0 +1,26 @@
+package com.mallang.subscribe.domain;
+
+import com.mallang.blog.domain.Blog;
+import com.mallang.common.domain.CommonDomainModel;
+import com.mallang.member.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class BlogSubscribe extends CommonDomainModel {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subscriber_id")
+    private Member subscriber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "blog_id")
+    private Blog blog;
+}
---
+++ src/main/java/com/mallang/subscribe/domain/BlogSubscribeRepository.java
@@ -0,0 +1,6 @@
+package com.mallang.subscribe.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogSubscribeRepository extends JpaRepository<BlogSubscribe, Long> {
+}
