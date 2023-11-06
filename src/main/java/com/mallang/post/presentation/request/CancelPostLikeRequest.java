2ea4059e90786211f9eaad2bb14228bf9163bba9
shin-mallang <huipulci1@naver.com>
2023-11-06 23:25:54 +0900

    [#12] feat: 포스트 좋아요 API 구현

---
+++ src/main/java/com/mallang/post/presentation/request/CancelPostLikeRequest.java
@@ -0,0 +1,12 @@
+package com.mallang.post.presentation.request;
+
+import com.mallang.post.application.command.CancelPostLikeCommand;
+
+public record CancelPostLikeRequest(
+        Long postId
+) {
+
+    public CancelPostLikeCommand toCommand(Long memberId) {
+        return new CancelPostLikeCommand(postId, memberId);
+    }
+}
