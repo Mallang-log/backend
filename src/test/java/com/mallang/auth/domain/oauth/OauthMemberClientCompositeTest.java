2a2b9ec70433945c3a1448351b950fefcf657fa9
shin-mallang <huipulci1@naver.com>
2023-11-11 23:14:18 +0900

    [#64] test: OauthMemberClientComposite 테스트 작성

---
+++ src/test/java/com/mallang/auth/domain/oauth/OauthMemberClientCompositeTest.java
@@ -0,0 +1,55 @@
+package com.mallang.auth.domain.oauth;
+
+import static com.mallang.auth.MemberFixture.동훈;
+import static com.mallang.auth.domain.OauthServerType.GITHUB;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.mallang.auth.domain.Member;
+import com.mallang.auth.domain.OauthServerType;
+import com.mallang.auth.exception.UnsupportedOauthTypeException;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("OauthMemberClientComposite 은(는)")
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class OauthMemberClientCompositeTest {
+
+    @Test
+    void 등록된_OauthMemberClient_중_처리가능한_Client_가_있으면_이를_사용하여_처리한다() {
+        // given
+        OauthMemberClient provider1 = new OauthMemberClient() {
+            @Override
+            public OauthServerType supportServer() {
+                return GITHUB;
+            }
+
+            @Override
+            public Member fetch(String code) {
+                return 동훈();
+            }
+        };
+        OauthMemberClientComposite composite = new OauthMemberClientComposite(Set.of(provider1));
+
+        // when
+        Member member = composite.fetch(GITHUB, "test");
+
+        // then
+        assertThat(member.getNickname()).isEqualTo("동훈");
+    }
+
+    @Test
+    void 등록된_OauthMemberClient_중_처리가능한_Client가_없으면_예외() {
+        // given
+        OauthMemberClientComposite composite = new OauthMemberClientComposite(Set.of());
+
+        // when  & then
+        assertThatThrownBy(() ->
+                composite.fetch(GITHUB, "test")
+        ).isInstanceOf(UnsupportedOauthTypeException.class);
+    }
+}
