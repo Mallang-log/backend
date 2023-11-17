0d4dbe19258d38b4f479b6d4ff815e1afb2ae1b1
shin-mallang <huipulci1@naver.com>
2023-11-17 13:03:48 +0900

    [#75] refactor: auth appliction layer kotlin으로 migration

--- src/main/kotlin/com/mallang/auth/application/OauthService.kt
+++ src/main/kotlin/com/mallang/auth/application/OauthService.kt
@@ -1,33 +1,31 @@
-package com.mallang.auth.application;
+package com.mallang.auth.application
 
-import com.mallang.auth.domain.Member;
-import com.mallang.auth.domain.MemberRepository;
-import com.mallang.auth.domain.OauthServerType;
-import com.mallang.auth.domain.oauth.AuthCodeRequestUrlProviderComposite;
-import com.mallang.auth.domain.oauth.OauthMemberClientComposite;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import com.mallang.auth.domain.Member
+import com.mallang.auth.domain.MemberRepository
+import com.mallang.auth.domain.OauthServerType
+import com.mallang.auth.domain.oauth.AuthCodeRequestUrlProviderComposite
+import com.mallang.auth.domain.oauth.OauthMemberClientComposite
+import org.springframework.stereotype.Service
 
-@RequiredArgsConstructor
 @Service
-public class OauthService {
+class OauthService(
+        private val memberRepository: MemberRepository,
+        private val oauthMemberClientComposite: OauthMemberClientComposite,
+        private val authCodeRequestUrlProviderComposite: AuthCodeRequestUrlProviderComposite
+) {
 
-    private final MemberRepository memberRepository;
-    private final OauthMemberClientComposite oauthMemberClientComposite;
-    private final AuthCodeRequestUrlProviderComposite authCodeRequestUrlProviderComposite;
-
-    public String getAuthCodeRequestUrl(OauthServerType oauthServerType) {
-        return authCodeRequestUrlProviderComposite.provide(oauthServerType);
+    fun getAuthCodeRequestUrl(oauthServerType: OauthServerType): String {
+        return authCodeRequestUrlProviderComposite.provide(oauthServerType)
     }
 
-    public Long login(OauthServerType oauthServerType, String authCode) {
-        Member member = oauthMemberClientComposite.fetch(oauthServerType, authCode);
-        Member find = memberRepository.findByOauthId(member.getOauthId())
-                .orElseGet(() -> signUp(member));
-        return find.getId();
+    fun login(oauthServerType: OauthServerType, authCode: String): Long {
+        val member = oauthMemberClientComposite.fetch(oauthServerType, authCode)
+        return memberRepository.findByOauthId(member.oauthId)
+                .orElseGet { signUp(member) }
+                .id
     }
 
-    private Member signUp(Member member) {
-        return memberRepository.save(member);
+    private fun signUp(member: Member): Member {
+        return memberRepository.save(member)
     }
 }
