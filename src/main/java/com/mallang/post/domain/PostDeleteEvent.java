023463f555700ce5bb564ac393f4a7f61cfad0b9
shin-mallang <huipulci1@naver.com>
2023-11-21 10:23:40 +0900

    [#42] feat: 이벤트 처리 로그 AOP 구현

--- src/main/java/com/mallang/post/domain/PostDeleteEvent.java
+++ src/main/java/com/mallang/post/domain/PostDeleteEvent.java
@@ -1,13 +1,19 @@
 package com.mallang.post.domain;
 
+import com.mallang.common.domain.DomainEvent;
 import java.time.LocalDateTime;
 
 public record PostDeleteEvent(
         Long postId,
         LocalDateTime deletedDate
-) {
+) implements DomainEvent {
 
     public PostDeleteEvent(Long postId) {
         this(postId, LocalDateTime.now());
+    }
+
+    @Override
+    public Long id() {
+        return postId();
     }
 }
