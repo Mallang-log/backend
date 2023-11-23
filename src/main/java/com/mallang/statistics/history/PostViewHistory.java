dcd6d0e6f8f0451a7d076274fe7a02796dde3fee
shin-mallang <huipulci1@naver.com>
2023-11-23 12:09:43 +0900

    [#13] feat: 포스트 조회수 이력 모델 작성

---
+++ src/main/java/com/mallang/statistics/history/PostViewHistory.java
@@ -0,0 +1,27 @@
+package com.mallang.statistics.history;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class PostViewHistory extends CommonHistory {
+
+    @Column(name = "uuid", nullable = false)
+    private UUID uuid;
+
+    @Column(nullable = false)
+    private Long postId;
+
+    public PostViewHistory(UUID uuid, Long postId, LocalDateTime createdDate) {
+        super(createdDate);
+        this.uuid = uuid;
+        this.postId = postId;
+    }
+}
---
+++ src/main/java/com/mallang/statistics/history/PostViewHistoryRepository.java
@@ -0,0 +1,6 @@
+package com.mallang.statistics.history;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostViewHistoryRepository extends JpaRepository<PostViewHistory, Long> {
+}
