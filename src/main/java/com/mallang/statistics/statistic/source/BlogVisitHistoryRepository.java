1df03124989801ec4ba3a2e22e9093ccfce7e1d1
shin-mallang <huipulci1@naver.com>
2023-11-28 17:05:39 +0900

    [#52] feat: 블로그 방문 이력 수집 job 작성

--- src/main/java/com/mallang/statistics/statistic/source/BlogVisitHistoryRepository.java
+++ src/main/java/com/mallang/statistics/statistic/source/BlogVisitHistoryRepository.java
@@ -5,6 +5,6 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlogVisitHistoryRepository extends JpaRepository<BlogVisitHistory, Long> {
-    
+
     Optional<BlogVisitHistory> findFirstByUuidAndBlogNameOrderByCreatedDateDesc(UUID uuid, String blogName);
 }
