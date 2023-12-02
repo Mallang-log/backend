f2ee8d41f6c2deaa8d1f004977321d87520ddd4d
shin-mallang <huipulci1@naver.com>
2023-12-03 06:54:35 +0900

    [#17] refactor: RestClient 빈으로 등록하여 사용

--- src/main/java/com/mallang/auth/infrastructure/oauth/HttpInterfaceConfig.java
+++ src/main/java/com/mallang/auth/infrastructure/oauth/HttpInterfaceConfig.java
@@ -1,14 +1,18 @@
 package com.mallang.auth.infrastructure.oauth;
 
 import com.mallang.auth.infrastructure.oauth.github.client.GithubApiClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
+@RequiredArgsConstructor
 @Configuration
 public class HttpInterfaceConfig {
+
+    private final RestClient restClient;
 
     @Bean
     public GithubApiClient googleApiClient() {
@@ -16,7 +20,6 @@ public class HttpInterfaceConfig {
     }
 
     private <T> T createHttpInterface(Class<T> clazz) {
-        RestClient restClient = RestClient.create();
         HttpServiceProxyFactory build = HttpServiceProxyFactory
                 .builderFor(RestClientAdapter.create(restClient)).build();
         return build.createClient(clazz);
