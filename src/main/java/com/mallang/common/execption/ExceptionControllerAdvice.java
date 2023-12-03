2cea4782b35cd8312d005db4c29cc886853ccf15
shin-mallang <huipulci1@naver.com>
2023-12-03 23:29:25 +0900

    feat: 일관된 예외처리를 위해 ControllerAdvice에서 ResponseEntityExceptionHandler 상속
    예를 들어 Method Not Allow의 경우, 405가 떠야 하지만, 현재 상황에서는 500이 떠버럼. 이를 해결

--- src/main/java/com/mallang/common/execption/ExceptionControllerAdvice.java
+++ src/main/java/com/mallang/common/execption/ExceptionControllerAdvice.java
@@ -1,17 +1,16 @@
 package com.mallang.common.execption;
 
 import static com.mallang.common.execption.ErrorCode.INTERNAL_SERVER_ERROR_CODE;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
-public class ExceptionControllerAdvice {
+public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(MallangLogException.class)
     public ResponseEntity<ErrorCode> handleException(MallangLogException e) {
@@ -20,15 +19,6 @@ public class ExceptionControllerAdvice {
         return ResponseEntity.status(errorCode.status())
                 .body(errorCode);
     }
-
-    @ExceptionHandler(ServletRequestBindingException.class)
-    public ResponseEntity<ErrorCode> handleException(ServletRequestBindingException e) {
-        ErrorCode errorCode = new ErrorCode(BAD_REQUEST, e.getMessage());
-        log.info("잘못될 요청 들어옴", e);
-        return ResponseEntity.status(errorCode.status())
-                .body(errorCode);
-    }
-
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorCode> handleException(Exception e) {
