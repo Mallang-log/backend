fe674a71b470dbce16b096d5e2e3bdc856657853
shin-mallang <huipulci1@naver.com>
2023-12-15 21:15:58 +0900

    [#160] test: 회원 조회 서비스 단위테스트로 변경

--- src/test/java/com/mallang/auth/query/MemberQueryServiceTest.java
+++ src/test/java/com/mallang/auth/query/MemberQueryServiceTest.java
@@ -1,13 +1,15 @@
 package com.mallang.auth.query;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 import com.mallang.auth.domain.Member;
 import com.mallang.auth.domain.OauthId;
 import com.mallang.auth.domain.OauthId.OauthServerType;
 import com.mallang.auth.domain.OauthMember;
+import com.mallang.auth.query.repository.MemberQueryRepository;
 import com.mallang.auth.query.response.MemberResponse;
-import com.mallang.common.ServiceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -16,23 +18,26 @@ import org.junit.jupiter.api.Test;
 @DisplayName("회원 조회 서비스 (MemberQueryService) 은(는)")
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(ReplaceUnderscores.class)
-class MemberQueryServiceTest extends ServiceTest {
+class MemberQueryServiceTest {
+
+    private final MemberQueryRepository memberQueryRepository = mock(MemberQueryRepository.class);
+    private final MemberQueryService memberQueryService = new MemberQueryService(memberQueryRepository);
 
     @Test
     void 회원정보를_조회한다() {
         // given
-        Member member = OauthMember.builder()
-                .nickname("mallang")
-                .profileImageUrl("profile")
-                .oauthId(new OauthId("test", OauthServerType.GITHUB))
-                .build();
-        Member saved = memberRepository.save(member);
+        Member member = new OauthMember(
+                "mallang",
+                "profile",
+                new OauthId("test", OauthServerType.GITHUB)
+        );
+        given(memberQueryRepository.getById(1L))
+                .willReturn(member);
 
         // when
-        MemberResponse memberResponse = memberQueryService.findProfile(saved.getId());
+        MemberResponse memberResponse = memberQueryService.findProfile(1L);
 
         // then
-        assertThat(memberResponse.id()).isEqualTo(saved.getId());
         assertThat(memberResponse.profileImageUrl()).isEqualTo("profile");
         assertThat(memberResponse.nickname()).isEqualTo("mallang");
     }
